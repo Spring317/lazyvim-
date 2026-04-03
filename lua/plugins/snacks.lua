63b0074f1a2e7acf1e067d6d0798ab46b92334ec
Spring317 <daoxuanquyhp@gmail.com>
2026-04-03 17:48:04 +0700

    Update: Less graphical effects

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,10 @@
+return {
+  "folke/snacks.nvim",
+  opts = {
+    animate = { enabled = false }, -- Disables window/split animations
+    scroll = { enabled = false }, -- Disables smooth scrolling (makes jumps instant)
+    indent = {
+      animate = { enabled = false }, -- Disables scope line animations
+    },
+  },
+}
