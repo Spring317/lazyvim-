97bbe20168c1dbfc2504e7dbe27685cba694f6ee
Spring317 <daoxuanquyhp@gmail.com>
2026-04-03 17:33:09 +0700

    Update: 3/4/2026 config. Enable CopilotChat and Autofill

---
+++ lua/plugins/copilot.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "zbirenbaum/copilot.lua",
+    opts = {
+      suggestion = {
+        enabled = true,
+        auto_trigger = true,
+        keymap = {
+          accept = "<Tab>",
+          accept_word = false,
+          accept_line = false,
+          next = "<M-]>",
+          prev = "<M-[>",
+          dismiss = "<C-]>",
+        },
+      },
+    },
+  },
+}
